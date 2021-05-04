0e659c131d627e03b1c363368a5badf07451af9c
Ajay Gangisetti <ajay.g415@gmail.com>
2021-05-04 08:40:26 +0530

    updating package in AccelerometerRecord

--- android/src/main/java/com/sensormanager/AccelerometerRecord.java
+++ android/src/main/java/com/sensormanager/AccelerometerRecord.java
@@ -6,7 +6,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
 
 import java.io.*;
 import java.util.Date;
@@ -54,7 +54,7 @@ public class AccelerometerRecord implements SensorEventListener {
 		}
     }
 
-	private void sendEvent(String eventName, @Nullable WritableMap params)
+	private void sendEvent(String eventName, @NonNull WritableMap params)
 	{
 		try {
 			mReactContext 
